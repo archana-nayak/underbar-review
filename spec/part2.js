af1ec7eb43a59823c4d54fef8ffb12f19dca5434
archana <archana.nayak@gmail.com>
2017-11-28 21:06:53 -0800

    "Add functionality for testing _.some, _.extend"

--- spec/part2.js
+++ spec/part2.js
@@ -195,7 +195,10 @@
       it('should override properties found on the destination', function() {
         // Replace this line with an `expect` statement that tests
         // the behavior described by the `it` string
-        throw new Error('This test is missing.');
+        // throw new Error('This test is missing.');
+        var destination = {'a' : 1};
+        var source = {'a' : 2};
+        expect(_.extend(destination, source)).to.eql(source);
       });
 
       it('should not override properties not found in the source', function() {
@@ -273,7 +276,10 @@
       it('should copy any property whose key is not already set on the target', function() {
         // Replace this line with an `expect` statement that tests
         // the behavior described by the `it` string
-        throw new Error('This test is missing.');
+        // throw new Error('This test is missing.');
+        var destination = {'a' : 1};
+        var source = {'b' : 2};
+        expect(_.defaults(destination,source)).to.eql({'a' : 1, 'b' : 2});
       });
 
       it('should not copy a property if that key is already set on the target', function() {
